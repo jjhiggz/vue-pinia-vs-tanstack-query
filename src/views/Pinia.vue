048d98f180ecb49501ad212c949e30075b6ccc15
Jon Higger <jonathan.higger@gmail.com>
2025-02-14 13:58:18 -0700

    starter

---
+++ src/views/Pinia.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h1>Pinia</h1>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  text-align: center;
+}
+</style>
